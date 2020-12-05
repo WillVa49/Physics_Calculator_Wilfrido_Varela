f46c1925c5a813f1f7cd201e13efe3dd90244c6d
Wilfrido Varela <wilfrido.varela@my.tccd.edu>
2020-12-05 21:45:44 +0000

    Added final comments

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
-//Practice with Functions   Wilfrido Varela, Dr_T   10/28/2020
+/* Wilfrido Varela  10/28/2020
+Dr_McMillan     Tarrant County College 
+Purpose of this assignment is to practice with functions
+Learned how to create a decision program with multiple functions.
+*/
 #include "Functions.h" //Reference to the header file
 
-
-int main()
-{
-  string weChoose = ""; //option local to int main()
-  string motionInput = ""; //option to choose from the motion menu
-  //do-while is used to loop until "E" or "e is pressed by the user. Exits the program."
-  do
-  {
-    showMenu();  //Calls the menu function
-    cout << "\nPlease enter one of the letters mentioned above: " << endl;
-    weChoose = validateString(weChoose);
-    chooseCalculation(weChoose); 
-    if (weChoose == "C" || weChoose == "c") //If the user chooses "C" or "c", the motion menu.
-    {
-      motionMenu();
-      cout << "\nWhat option do want to choose: " << endl;  
-      motionInput = validateString(motionInput);
-      chooseMotion(motionInput);
-    }
-  }while (weChoose != "E" && weChoose != "e");  //DeMorgan's Law is applied here
+int main() {
+	string weChoose = "";	// option local to int main()
+	string motionInput = ""; // option to choose from the motion menu
+	// do-while is used to loop until "E" or "e is pressed by the user. Exits
+	// the program."
+	do {
+		showMenu(); // Calls the menu function
+		cout << "\nPlease enter one of the letters mentioned above: " << endl;
+		weChoose = validateString(weChoose);
+		chooseCalculation(weChoose);
+		if (weChoose == "C" ||
+			weChoose == "c") // If the user chooses "C" or "c", the motion menu.
+		{
+			motionMenu();
+			cout << "\nWhat option do want to choose: " << endl;
+			motionInput = validateString(motionInput);
+			chooseMotion(motionInput);
+		}
+	} while (weChoose != "E" &&
+			 weChoose != "e"); // DeMorgan's Law is applied here
 }
